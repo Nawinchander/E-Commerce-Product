8212b42e9b1d5de533ebc2213909fb95d0604425
Nawin chander <nawinnapzy289@gmail.com>
2026-04-25 11:23:25 +0530

    add

--- ecom/src/Client/src/Component/ProductList.js
+++ ecom/src/Client/src/Component/ProductList.js
@@ -1,5 +1,7 @@
+
 import React, { useEffect, useState } from "react";
 import api from "../api";
+import ProductCard from "./ProductCard";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
@@ -10,15 +12,37 @@ function ProductList() {
 
   return (
     <div>
-      <h2>Products</h2>
       {products.map(product => (
-        <div key={product._id}>
-          {product.name} - ₹{product.price}
-        </div>
+        <ProductCard key={product._id} product={product} />
       ))}
     </div>
   );
 }
 
 export default ProductList;
+
+
+// import React, { useEffect, useState } from "react";
+// import api from "../api";
+
+// function ProductList() {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     api.get("/products").then(res => setProducts(res.data));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Products</h2>
+//       {products.map(product => (
+//         <div key={product._id}>
+//           {product.name} - ₹{product.price}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default ProductList;
 
