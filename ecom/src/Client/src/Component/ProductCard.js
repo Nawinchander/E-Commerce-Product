11cfd48de660fb13eec89ce37c8ad8d0efbc7f8c
Nawin chander <nawinnapzy289@gmail.com>
2026-04-25 11:21:34 +0530

    add

--- ecom/src/Client/src/Component/ProductCard.js
+++ ecom/src/Client/src/Component/ProductCard.js
@@ -1,2 +1,16 @@
 //// client/src/components/ProductCard.js
 
+import React from "react";
+
+function ProductCard({ product }) {
+  return (
+    <div style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}>
+      <h3>{product.name}</h3>
+      <p>₹{product.price}</p>
+      <p>{product.category}</p>
+    </div>
+  );
+}
+
+export default ProductCard;
+
